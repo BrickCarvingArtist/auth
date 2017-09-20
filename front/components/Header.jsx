8cffdd29801fff6abe75a197e4a93b97dff3f962
pengcheng.yang <806321554@qq.com>
2017-09-20 11:46:08 +0800

    copy the header component from punchy

--- front/components/Header.jsx
+++ front/components/Header.jsx
@@ -1,23 +1,60 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
+import classNames from "classnames";
 process.title === "node" || require("../styles/header.styl");
+const LEVELS = ["normal", "blue", "red"];
+const Button = ({icon, label, to, onClick = function(){}, level = 0}) => {
+	if(icon){
+		if(to){
+			return (
+				<Link to={to}>
+					<icon className={
+						classNames("medium", icon)
+					}></icon>
+				</Link>
+			);
+		}
+		return <icon className={
+			classNames("medium", icon)
+		} onClick={onClick}></icon>;
+	}
+	if(to){
+		return <Link to={to} className={
+			classNames(LEVELS[level])
+		}>{label}</Link>;
+	}
+	if(onClick){
+		return <a onClick={onClick} className={
+			classNames(LEVELS[level])
+		}>{label}</a>
+	}
+	return <icon></icon>;
+};
 @withRouter
 @connect(({core}) => ({
-	title: core.title
+	title: core.title,
+	headerLeftButton: core.headerLeftButton,
+	headerRightButton: core.headerRightButton
 }))
 export default class Header extends Component{
 	render(){
-		const {history} = this.props;
+		const {
+			history,
+			headerRightButton
+		} = this.props;
+		let {headerLeftButton} = this.props;
+		headerLeftButton === "back" && (headerLeftButton = {
+			icon: "medium back",
+			onClick(){
+				history.goBack();
+			}
+		});
 		return (
 			<header>
-				<icon className="back" onClick={
-					() => {
-						history.goBack();
-					}
-				}></icon>
+				<Button {...headerLeftButton} />
 				<strong>{this.props.title}</strong>
-				<icon></icon>
+				<Button {...headerRightButton} />
 			</header>
 		);
 	}
