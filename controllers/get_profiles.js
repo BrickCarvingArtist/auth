b5831bd6a28a282db175dfe66e2b1a28b36422a2
pengcheng.yang <806321554@qq.com>
2017-09-26 20:38:58 +0800

    miss to parse the querystring

--- controllers/get_profiles.js
+++ controllers/get_profiles.js
@@ -3,8 +3,9 @@ import {success, error} from "../utils";
 import {getProfiles} from "../services/user";
 export default () => async ctx => {
 	try{
-		const {user_id} = ctx.query;
-		ctx.body = success(await getProfiles(Array.isArray(user_id) ? user_id : [user_id]));
+		const {user_id} = ctx.query,
+			parsedUserId = parse(user_id).user_id;
+		ctx.body = success(await getProfiles(Array.isArray(parsedUserId) ? parsedUserId : [user_id]));
 	}catch(e){
 		ctx.body = error({
 			code: 5000000800,
