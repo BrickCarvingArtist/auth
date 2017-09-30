8eeef2f554658a53f2301e4347962bf0912f2215
pengcheng.yang <806321554@qq.com>
2017-09-30 17:36:07 +0800

    add an auto sign in when sign up succeed

--- controllers/sign_up/index.js
+++ controllers/sign_up/index.js
@@ -1,12 +1,22 @@
 import {success, error} from "../../utils";
-import {signUp} from "../../services/user";
+import {signUp, signIn} from "../../services/user";
 export default () => async ctx => {
 	const {user, password} = ctx.request.body;
 	try{
-		ctx.body = success(await signUp({
+		let referer = await signUp({
 			tel: user,
 			password
-		}));
+		});
+		const ssoToken = await signIn({
+			tel: user,
+			password
+		});
+		ctx.cookies.set("sso_token", ssoToken, {
+			maxAge: 60 * 60 * 24 * 1000
+		});
+		referer = new URL(referer || "https://punchy.ikindness.cn/");
+		referer.searchParams.append("sso_token", ssoToken);
+		ctx.body = success(referer.href);
 	}catch(e){
 		ctx.body = error({
 			code: e.code || 5000000301,
